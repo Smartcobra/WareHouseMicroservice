b1a09f10cd8584ac7fceb7f8cd0279e902d161b3
jitu <jitu@gmail.com>
2020-08-12 23:34:30 +0530

    commit

---
+++ UomApp/src/main/java/in/jit/controller/UomRestController.java
@@ -0,0 +1,55 @@
+package in.jit.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.jit.model.Uom;
+import in.jit.model.UomVO;
+import in.jit.service.UomService;
+
+@RestController
+@RequestMapping("/rest/uom")
+public class UomRestController {
+
+	@Autowired
+	private UomService service;
+
+	@GetMapping("/all")
+	public ResponseEntity<List<UomVO>> getUomToPart() {
+		UomVO uomVO =null;
+		List<UomVO> listUomVO = new ArrayList<UomVO>();;
+		int index = 0;
+
+		List<Uom> allUoms = service.getAllUoms();
+		for (Uom uom : allUoms) {
+			uomVO= new UomVO();
+			uomVO.setId(uom.getId());
+			// System.out.println(uom.getId());
+			uomVO.setUomModel(uom.getUomModel());
+			// System.out.println(uom.getUomModel());
+			uomVO.setUomType(uom.getUomType());
+			// System.out.println(uom.getUomType());
+			// System.out.println(uomVO);
+			//listUomVO.set(index, uomVO);
+			
+			listUomVO.add(index, uomVO);
+			index++;
+
+		}
+		/*
+		 * for (int i = 0; i < listUomVO.size(); i++) {
+		 * System.out.println(listUomVO.get(i).getId()); }
+		 */
+		// listUomVO.forEach(System.out::println);
+		return new ResponseEntity<List<UomVO>>(listUomVO, HttpStatus.OK);
+
+	}
+
+}
---
+++ UomApp/src/main/java/in/jit/model/UomVO.java
@@ -0,0 +1,38 @@
+package in.jit.model;
+
+
+
+public class UomVO {
+
+	private Integer id;
+	private String uomType;
+	private String uomModel;
+	
+	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUomType() {
+		return uomType;
+	}
+
+	public void setUomType(String uomType) {
+		this.uomType = uomType;
+	}
+
+	public String getUomModel() {
+		return uomModel;
+	}
+
+	public void setUomModel(String uomModel) {
+		this.uomModel = uomModel;
+	}
+	
+	
+}

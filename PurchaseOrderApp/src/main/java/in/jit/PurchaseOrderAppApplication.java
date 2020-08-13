4aaff3f3b0bd4d6e3770a3ba8307a0f7097c0d1e
jitu <jitu@gmail.com>
2020-08-13 21:53:37 +0530

    purchase order

---
+++ PurchaseOrderApp/src/main/java/in/jit/PurchaseOrderAppApplication.java
@@ -0,0 +1,13 @@
+package in.jit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseOrderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseOrderAppApplication.class, args);
+	}
+
+}

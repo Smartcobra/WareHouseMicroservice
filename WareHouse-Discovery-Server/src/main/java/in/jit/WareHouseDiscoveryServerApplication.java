84f1ea768cee5382690a422ae7a502da9da37b36
jitu <jitu@gmail.com>
2020-08-25 23:08:16 +0530

    commit

---
+++ WareHouse-Discovery-Server/src/main/java/in/jit/WareHouseDiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package in.jit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareHouseDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WareHouseDiscoveryServerApplication.class, args);
+	}
+
+}

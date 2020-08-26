a2884a791d313e3a3fc94e97c54e2a1f993a843a
jitu <jitu@gmail.com>
2020-08-26 09:46:09 +0530

    commi

---
+++ WareHouse-Zuul-GateWay/src/main/java/in/jit/WareHouseZuulGateWayApplication.java
@@ -0,0 +1,13 @@
+package in.jit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WareHouseZuulGateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WareHouseZuulGateWayApplication.class, args);
+	}
+
+}

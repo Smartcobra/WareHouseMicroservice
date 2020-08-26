04c7fce55810396ee82c3d99808c5124a38b4ff4
jitu <jitu@gmail.com>
2020-08-26 22:58:45 +0530

    commit

--- WareHouse-Zuul-GateWay/src/main/java/in/jit/WareHouseZuulGateWayApplication.java
+++ WareHouse-Zuul-GateWay/src/main/java/in/jit/WareHouseZuulGateWayApplication.java
@@ -2,8 +2,12 @@ package in.jit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
 @SpringBootApplication
+@EnableZuulProxy
 public class WareHouseZuulGateWayApplication {
 
 	public static void main(String[] args) {
